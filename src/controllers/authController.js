d0b1c21fe402aaa06d8f51c9627334e6d448485e
Intern-Yago <yagovictorbotafogo@gmail.com>
2022-11-02 17:22:03 -0300

    node auth

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,16 @@
 const express = require("express")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
+require('dotenv').config()
+
 const User = require("../models/user")
+
+function generateToken(params={}){
+    return jwt.sign(params, process.env.SECRET_KEY, {
+        expiresIn: 86400,
+    })
+}
 
 const router = express.Router()
  router.post('/register', async (req,res)=>{
@@ -12,11 +23,33 @@ const router = express.Router()
 
         user.password = undefined
 
-        return res.send({user})
+        return res.send({
+            user,
+            token: generateToken({id:user.id})
+        })
     }
     catch(err){
         return res.status(400).send({error: 'Registration failed'})
     }
  } )
+
+ router.post("/authentication", async(req,res)=>{
+    const {email, password} = req.body
+
+    const user = await User.findOne({email}).select('+password')
+
+    if(!user){
+        return res.status(400).send({error: "User not found"})
+    }
+    if(!await bcrypt.compare(password, user.password))
+        return res.status(400).send({error:"Invalid password"})
+
+    user.password = undefined
+
+    res.send({
+        user, 
+        token: generateToken({id:user.id})
+    })
+ })
 
  module.exports = (app) => app.use('/auth', router)
